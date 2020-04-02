6f70fd69cff66b96b8a6dad4750e2dbbd832367f
Caroline Glass <carolineglass89@gmail.com>
2020-04-02 21:50:31 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,10 @@
 def find_max_value(array)
   count = 0
+  sorted_array = []
   while count < array.length do 
-    
+    push.sorted_array(array[count])
     count += 1
+  sorted_array = sorted_array.sort
   end 
+  sorted_array[-1]
 end
